a4db23666493eaf89974f0fe79b4bcf4b0888d2a
Willy Micieli <otechdo@otechdo.com>
2024-10-13 05:10:48 +0200

    improve interface

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 use crossterm::event::{self, Event, KeyCode};
 use ratatui::backend::CrosstermBackend;
 use ratatui::layout::{Alignment, Constraint, Direction, Layout};
-use ratatui::style::{Color, Modifier, Style};
-use ratatui::widgets::{Block, Borders};
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::widgets::{Block, BorderType, Borders, Padding};
 use ratatui::{CompletedFrame, Terminal};
+use std::fmt::{Display, Formatter};
 use std::io::Stdout;
 use tui_textarea::TextArea;
+
 enum PageIndex {
     Page0,
     Page1,
@@ -19,49 +21,86 @@ enum PageIndex {
     Page6,
 }
 
+impl Display for PageIndex {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PageIndex::Page0 => {
+                write!(f, " /dev/null <== page 1/7 ==> Resolution ")
+            }
+            PageIndex::Page1 => {
+                write!(f, " Problematics <== page 2/7 ==> Security ")
+            }
+            PageIndex::Page2 => {
+                write!(f, " Resolution <== page 3/7 ==> Tests ")
+            }
+            PageIndex::Page3 => {
+                write!(f, " Security <== page 4/7 ==> Requirements ")
+            }
+            PageIndex::Page4 => {
+                write!(f, " Tests <== page 5/7 ==> Database ")
+            }
+            PageIndex::Page5 => {
+                write!(f, " Requirements <== page 6/7 ==> Communication ")
+            }
+            PageIndex::Page6 => {
+                write!(f, " Database <== page 7/7 ==> /dev/null ")
+            }
+        }
+    }
+}
+
 struct Page {
     main_title: &'static str,
     areas: [TextArea<'static>; 4],
     current_page: PageIndex,
     titles: [&'static str; 4],
+    describe: [&'static str; 4],
 }
 
 #[derive(Copy, Clone)]
-struct App {
-    current_page: usize,
-    max_pages: usize,
-}
+struct App {}
 impl App {
-    fn new(max_pages: usize) -> Self {
-        Self {
-            current_page: 0,
-            max_pages,
-        }
+    fn new() -> Self {
+        Self {}
     }
     fn render_commit<'a>(
         self,
         rei: &'a mut Terminal<CrosstermBackend<Stdout>>,
         areas: &mut Vec<TextArea>,
+        describe: &mut Vec<&'static str>,
         titles: &mut Vec<&'static str>,
         main_title: &'static str,
+        index: &PageIndex,
+        witch: usize,
     ) -> std::io::Result<CompletedFrame<'a>> {
         for (i, area) in areas.iter_mut().enumerate() {
             area.set_block(
                 Block::default()
                     .borders(Borders::all())
-                    .style(Style::default())
+                    .title_alignment(Alignment::Left)
                     .title(format!(" {} ", titles[i])),
             );
+            area.set_line_number_style(Style::default().fg(Color::White));
+            area.set_cursor_style(Style::underlined(Style::default().fg(Color::White)));
+            if i.eq(&witch) {
+                activate(area, titles[i], describe[i]);
+            } else {
+                inactivate(area, titles[i], describe[i]);
+            }
         }
         rei.draw(|f| {
             let parent_block = Block::default()
                 .title(format!(" {main_title} "))
                 .title_alignment(Alignment::Center)
+                .title_bottom(format!("{index}"))
+                .title_alignment(Alignment::Center)
                 .borders(Borders::all());
 
             let chunks = Layout::default()
                 .direction(Direction::Horizontal)
-                .margin(1)
+                .vertical_margin(2)
+                .horizontal_margin(4)
+                .spacing(2)
                 .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
                 .split(f.area());
 
@@ -75,138 +114,103 @@ impl App {
                 .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
                 .split(chunks[1]);
             f.render_widget(parent_block, f.area());
-            if self.current_page == 0 {
-                f.render_widget(&areas[0], left_chunks[0]);
-                f.render_widget(&areas[2], left_chunks[1]);
+            f.render_widget(&areas[0], left_chunks[0]);
+            f.render_widget(&areas[2], left_chunks[1]);
 
-                f.render_widget(&areas[1], right_chunks[0]);
-                f.render_widget(&areas[3], right_chunks[1]);
-            } else {
-                let x = self.current_page; // 1
-                let a = x * 2 + 2; // 1 *2 +2 = 4
-                let b = a + 2; // 4+2 =6
-
-                let c = x * 2 + 3; // 1 *2 +3 = 5
-                let d = a + 3; // 4+3 =7;
-                f.render_widget(&areas[a], left_chunks[0]);
-                f.render_widget(&areas[b], left_chunks[1]);
-
-                f.render_widget(&areas[c], right_chunks[0]);
-                f.render_widget(&areas[d], right_chunks[1]);
-            }
+            f.render_widget(&areas[1], right_chunks[0]);
+            f.render_widget(&areas[3], right_chunks[1]);
         })
     }
-    fn next_page(&mut self) -> &mut Self {
-        if self.current_page == self.max_pages {
-            self
-        } else {
-            self.current_page += 1;
-            self
-        }
-    }
-
-    fn prev_page(&mut self) -> &mut Self {
-        if self.current_page.gt(&0) {
-            self.current_page -= 1;
-        }
-        self
-    }
 }
-fn inactivate(textarea: &mut TextArea<'_>, title: &str) {
+fn inactivate(textarea: &mut TextArea<'_>, title: &str, describe: &str) {
     textarea.set_cursor_line_style(Style::default());
     textarea.set_cursor_style(Style::default());
     textarea.set_block(
         Block::default()
             .borders(Borders::all())
+            .border_type(BorderType::Rounded)
             .style(Style::default().fg(Color::DarkGray))
-            .title(format!(" {title} ")),
+            .title(format!(" {title} "))
+            .title_bottom(format!(" {describe} ")),
     );
 }
-fn update(witch: usize, textarea: &mut [TextArea], titles: &[&str]) {
-    assert_eq!(textarea.len(), titles.len());
-    for x in 0..textarea.len() {
-        if x.ne(&witch) {
-            inactivate(&mut textarea[x], titles[x]);
+fn update(witch: usize, page: &mut Page) {
+    for (i, area) in page.areas.iter_mut().enumerate() {
+        if witch.eq(&i) {
+            activate(area, page.main_title, page.describe[i]);
         } else {
-            activate(&mut textarea[x], titles[witch]);
+            inactivate(area, page.main_title, page.describe[i]);
         }
     }
 }
-fn activate(textarea: &mut TextArea<'_>, title: &str) {
-    textarea.set_cursor_line_style(Style::default().add_modifier(Modifier::UNDERLINED));
-    textarea.set_cursor_style(Style::default().add_modifier(Modifier::REVERSED));
+fn activate(textarea: &mut TextArea<'_>, title: &str, describe: &str) {
     let mut lines: Vec<usize> = Vec::new();
     textarea.lines().iter().for_each(|line| {
         lines.push(line.chars().count());
     });
     let block = Block::default()
-        .borders(Borders::ALL)
-        .title_alignment(Alignment::Center);
+        .rapid_blink()
+        .borders(Borders::all())
+        .border_type(BorderType::Rounded)
+        .padding(Padding::new(0, 2, 0, 2))
+        .title_alignment(Alignment::Center)
+        .title(format!(" {title} "))
+        .title_bottom(format!(" {describe} "));
     let mut red = false;
-    let mut blue = false;
-    let mut cyan = false;
+    let mut orange = false;
+    let mut yellow = false;
     let mut green = false;
     for line in &lines {
         if line.gt(&72) {
             red = true;
-            blue = false;
-            cyan = false;
+            orange = false;
+            yellow = false;
             green = false;
         } else if line.le(&35) && line.ge(&20) {
-            blue = true;
-        } else if line.le(&50) && line.ge(&35) {
-            cyan = true;
-        } else if line.lt(&72) && line.ge(&50) {
             green = true;
+        } else if line.le(&50) && line.ge(&35) {
+            yellow = true;
+        } else if line.lt(&72) && line.ge(&50) {
+            orange = true;
         }
     }
     if red {
         textarea.set_block(
             block
-                .borders(Borders::all())
                 .style(Style::default().fg(Color::Red))
                 .border_style(Style::default().fg(Color::Red))
                 .title(format!(
                     " {title} ( a line is superior to the max lines length )"
                 )),
         );
-    } else if blue {
+    } else if yellow {
         textarea.set_block(
             block
-                .borders(Borders::all())
-                .style(Style::default().fg(Color::Blue))
-                .border_style(Style::default().fg(Color::Blue))
-                .title(format!(" {title} ")),
+                .style(Style::default().fg(Color::Yellow))
+                .border_style(Style::default().fg(Color::Yellow)),
         );
-    } else if cyan {
+    } else if orange {
         textarea.set_block(
             block
-                .borders(Borders::all())
-                .style(Style::default().fg(Color::Cyan))
-                .border_style(Style::default().fg(Color::Cyan))
-                .title(format!(" {title} ")),
+                .style(Style::default().fg(Color::Rgb(255, 165, 0)))
+                .border_style(Style::default().fg(Color::Rgb(255, 165, 0))),
         );
     } else if green {
         textarea.set_block(
             block
-                .borders(Borders::all())
                 .style(Style::default().fg(Color::Green))
-                .border_style(Style::default().fg(Color::Green))
-                .title(format!(" {title} ")),
+                .border_style(Style::default().fg(Color::Green)),
         );
     } else {
         textarea.set_block(
             block
-                .borders(Borders::all())
                 .style(Style::default().fg(Color::White))
-                .border_style(Style::default().fg(Color::White))
-                .title(format!(" {title} ")),
+                .border_style(Style::default().fg(Color::White)),
         );
     }
 }
-fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
-    let app = App::new(6);
-    let pages = [
+fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>, app: App) -> std::io::Result<()> {
+    let mut pages = [
         Page {
             main_title: "Problematic",
             areas: [
@@ -217,6 +221,12 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
             ],
             current_page: PageIndex::Page0,
             titles: ["Title", "Description", "Steps to reproduce", "Expectation"],
+            describe: [
+                "Indicate the problem title",
+                "Describe the problem in detail",
+                "Indicate the steps necessary to reproduce the problem",
+                "Describe the expected behavior",
+            ],
         },
         Page {
             main_title: "Resolution",
@@ -227,7 +237,13 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
                 TextArea::default(),
             ],
             current_page: PageIndex::Page1,
-            titles: ["Before", "After", "Samples", "Results"],
+            titles: ["Before", "After", "Results", "Samples"],
+            describe: [
+                "Describe the state before the implementation of the resolution",
+                "Describe the state after the implementation of the resolution",
+                "Describe the results obtained after the implementation",
+                "Give examples of the use of the resolution",
+            ],
         },
         Page {
             main_title: "Security",
@@ -239,6 +255,12 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
             ],
             current_page: PageIndex::Page2,
             titles: ["Vulnerability", "Quality", "Conformity", "Risk"],
+            describe: [
+                "Describe potential security vulnerabilities",
+                "Describe the quality aspects of the code and the solution",
+                "Indicate the security and conformity standards met",
+                "Describe potential security risks",
+            ],
         },
         Page {
             main_title: "Tests",
@@ -249,7 +271,13 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
                 TextArea::default(),
             ],
             current_page: PageIndex::Page3,
-            titles: ["Added", "Updated", "Removed", "Platforms"],
+            titles: ["Added", "Updated", "Deleted", "Platforms"],
+            describe: [
+                "Describe the new tests added",
+                "Describe the updated tests",
+                "Describe the deleted tests",
+                "Indicate the platforms on which the tests were carried out",
+            ],
         },
         Page {
             main_title: "Requirements",
@@ -260,10 +288,16 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
                 TextArea::default(),
             ],
             current_page: PageIndex::Page4,
-            titles: ["Database", "Dependencies", "Packages", "Rollback"],
+            titles: ["Breaking changes", "New needed dependencies", "New packages needed", "Rollback"],
+            describe: [
+                "Indicate if the code have a breaking changes",
+                "Indicate the new needed dependencies",
+                "Indicate the new needed packages name's",
+                "Describe the rollback process in case of a problem",
+            ],
         },
         Page {
-            main_title: "Timeline",
+            main_title: "Database",
             areas: [
                 TextArea::default(),
                 TextArea::default(),
@@ -271,7 +305,13 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
                 TextArea::default(),
             ],
             current_page: PageIndex::Page5,
-            titles: ["Phase", "Results", "Status", "Updates"],
+            titles: ["Up", "Down", "Changes", "Why"],
+            describe: [
+                "What's it's created on up",
+                "What's it's removed on down",
+                "Describe the migrations results",
+                "Describe the reason of the update",
+            ],
         },
         Page {
             main_title: "Communication",
@@ -283,12 +323,19 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
             ],
             current_page: PageIndex::Page6,
             titles: ["Authors", "Testers", "Comments", "Notes"],
+            describe: [
+                "Authors name's",
+                "Indicate the project testers",
+                "Indicate comments and feedback",
+                "Indicate important remarks and observations",
+            ],
         },
     ];
+    let mut page: usize = 0;
     let mut witch: usize = 0;
 
     loop {
-        match pages[witch].current_page {
+        match pages[page].current_page {
             PageIndex::Page0
             | PageIndex::Page1
             | PageIndex::Page2
@@ -299,9 +346,12 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
                 assert!(app
                     .render_commit(
                         rei,
-                        &mut pages[witch].areas.to_vec(),
-                        &mut pages[witch].titles.to_vec(),
-                        pages[witch].main_title
+                        &mut pages[page].areas.to_vec(),
+                        &mut pages[page].describe.to_vec(),
+                        &mut pages[page].titles.to_vec(),
+                        pages[page].main_title,
+                        &pages[page].current_page,
+                        witch,
                     )
                     .is_ok());
             }
@@ -310,33 +360,48 @@ fn commit(rei: &mut Terminal<CrosstermBackend<Stdout>>) -> std::io::Result<()> {
             if key.code == KeyCode::Esc {
                 break;
             } else if key.code == KeyCode::PageUp {
-                if witch.lt(&(pages.len() - 1)) {
-                    witch += 1;
+                witch = 0;
+                if page.lt(&(pages.len() - 1)) {
+                    page += 1;
                 }
             } else if key.code == KeyCode::PageDown {
+                witch = 0;
+                if page.gt(&0) {
+                    page -= 1;
+                }
+            } else if key.code == KeyCode::F(7) {
+                if witch.lt(&3) {
+                    witch += 1;
+                    update(witch, &mut pages[page])
+                }
+            } else if key.code == KeyCode::F(5) {
                 if witch.gt(&0) {
                     witch -= 1;
                 }
+            } else {
+                pages[page].areas.get_mut(witch).expect("").input(key);
             }
         }
     }
     Ok(())
 }
 
-fn dojo(rei: &mut Terminal<CrosstermBackend<Stdout>>) {
+fn dojo(rei: &mut Terminal<CrosstermBackend<Stdout>>, app: App) {
     loop {
         if let Event::Key(key) = event::read().unwrap() {
             if key.code == KeyCode::Esc {
                 break;
             }
-            if key.code == KeyCode::F(2) && commit(rei).is_ok() {
+            if key.code == KeyCode::F(2) && commit(rei, app).is_ok() {
                 break;
             }
         }
     }
 }
 fn main() {
+    let app = App::new();
+
     let mut rei = ratatui::init();
-    dojo(&mut rei);
+    dojo(&mut rei, app);
     ratatui::restore();
 }
